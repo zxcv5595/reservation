73fe90c29867f03c6989975a31bd968b94089cd8
zxcv5595 <zxcv5595@korea.ac.kr>
2023-05-15 23:22:22 +0900

    feat : [001-START-PROJECT]: 프로젝트 생성 및 구성 - 프로젝트 생성 - SecurityConfig를 통해 접속가능 해야하는 url 설정 - swagger를 이용한 doc 작성 (config 추가예정) - docker를 이용해 데이터베이스 생성, mysql.sh - 필요한 application.properties 작성

---
+++ user/src/main/java/com/zxcv5595/reservation/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.zxcv5595.reservation.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**/signup", "/**/signin").permitAll();
+    }
+
+    @Override
+    public void configure(final WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/swagger-ui/**");
+    }
+
+}
