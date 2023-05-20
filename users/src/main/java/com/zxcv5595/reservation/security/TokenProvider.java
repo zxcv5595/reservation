e612cd8cae6988521bf6a7dd3bf9ec73fb4b2f6c
zxcv5595 <zxcv5595@korea.ac.kr>
2023-05-21 01:16:49 +0900

    feat : [005-REFACTORING]: 리펙토링 계획변경 - 회원관리 모듈을 나눠서 진행 - 회원관리 모듈 구현

--- users/src/main/java/com/zxcv5595/reservation/security/TokenProvider.java
+++ users/src/main/java/com/zxcv5595/reservation/security/TokenProvider.java
@@ -1,20 +1,18 @@
 package com.zxcv5595.reservation.security;
 
-import com.zxcv5595.reservation.service.UserService;
-import com.zxcv5595.reservation.type.Role;
+import com.zxcv5595.reservation.service.OwnerService;
+import com.zxcv5595.reservation.type.UserType;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 @Component
@@ -23,11 +21,11 @@ public class TokenProvider {
 
     private static final String KEY_ROLES = "roles";
     private static final long EXPIRED_TIME = 1000 * 60 * 60; // 1 hour
-    private final UserService userService;
+    private final OwnerService ownerService;
     @Value("${spring.jwt.secret}")
     private String secretKey;
 
-    public String generateToken(String username, Set<Role> userType) {
+    public String generateToken(String username, UserType userType) {
         Claims claims = Jwts.claims().setSubject(username);
         claims.put(KEY_ROLES, userType);
 
@@ -42,10 +40,11 @@ public class TokenProvider {
                 .compact();
     }
 
-    @Transactional
     public Authentication getAuthentication(String jwt) {
+        String username = getUsername(jwt);
+        UserType userType = getUserType(jwt);
 
-        UserDetails userDetails = userService.loadUserByUsername(getUsername(jwt));
+        UserDetails userDetails = ownerService.loadUserByUsername(username);
 
         return new UsernamePasswordAuthenticationToken(userDetails, "",
                 userDetails.getAuthorities());
@@ -53,6 +52,12 @@ public class TokenProvider {
 
     public String getUsername(String token) {
         return paresClaims(token).getSubject();
+    }
+
+    private UserType getUserType(String token) {
+        Claims claims = paresClaims(token);
+        String userType = claims.get(KEY_ROLES, String.class);
+        return UserType.valueOf(userType);
     }
 
     public boolean validateToken(String token) {
